34238695f0e9e3a473daeeaf93364131c4dcb4a0
astrasb <astra.bryant@gmail.com>
2020-08-10 13:56:32 -0700

    Changing how topTable data is merged with expression datat to maintain the logFC ordering of the topTable

--- Stercoralis_RNAseq_Browser/Server/limma_ranking.R
+++ Stercoralis_RNAseq_Browser/Server/limma_ranking.R
@@ -94,7 +94,7 @@ limma_ranking <- function(comparison, targetStage, contrastStage, multipleCorrec
             dplyr::select(geneID, starts_with(paste0(tS,"-")), 
                           starts_with(paste0(cS,"-"))) %>%
             left_join(groupAvgs, by = "geneID") %>%
-            left_join(list.highlight.df[[y]], by = "geneID") %>%
+            left_join(list.highlight.df[[y]],., by = "geneID") %>%
             left_join(dplyr::select(diffDesc,geneID,comparison[y]), by = "geneID") %>%
             dplyr::rename(DEG_Desc=comparison[y]) %>%
             dplyr::relocate(DEG_Desc)
